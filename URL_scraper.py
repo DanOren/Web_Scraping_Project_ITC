e2654fe02192508d30e30b8300bd41132e54b610
Dan Oren <danoren90@gmail.com>
2021-03-05 16:24:14 +0200

    added TV_show scrapper method, takes about 2 mins to scrape the example website for tv shows.

--- URL_scraper.py
+++ URL_scraper.py
@@ -88,7 +88,7 @@ class Scrapper:
         for index, url in enumerate(self.url_list):
             self.movie_data_scraper(url)
 
-    def movie_data_scraper(self, input_url):
+    def movie_data_scraper(self, input_url):  # TODO: this is for tests on on other types of web scrapers
         """
         For each url extracted by search_pages_url, this method extracts film name and director.
         works on 1 url at a time
@@ -108,27 +108,32 @@ class Scrapper:
                 item_info['User score'] = user_ttl_score.text
             for title in soup.find_all('h1'):
                 item_info['Title'] = title.text
-            release_year_expression = re.compile('release_year')  # expression to find the largest score
-            for release_year in soup.find_all('span', class_=release_year_expression, limit=1):
+            # TODO:the year loop need to fix to take only second element
+            for release_year in soup.find('span', class_='release_date').find_all('span'):
                 item_info['Release Year'] = release_year.text
             studio_expression = re.compile('/company')  # expression to find the studio
             for studio in soup.find_all('a', href=studio_expression, limit=1):
                 item_info['Studio'] = studio.text
-            # dir_expression = re.compile('/person')
-            for director in soup.find_all('div', class_='director', limit=1):
-                # something = director.find('a', href=True) # TODO: delete me
-                item_info['Director'] = list(list(director.children)[3].children)[0].text
-            for rating in soup.find_all('div', class_='rating', limit=1):
-                item_info['Rating'] = list(rating.children)[3].text.strip()
-            for runtime in soup.find_all('div', class_='runtime', limit=1):
-                item_info['Runtime'] = list(runtime.children)[3].text
+            # TODO: Need to check multiple creators
+            for creator in soup.find_all('div', class_='creator', limit=1):
+                item_info['Creator'] = list(list(creator.children)[3].children)[0].text
+            genre_list = []
+            for index, genre in enumerate(soup.find('div', class_='genres').find_all('span')):
+                if index != 0 and index != 1:  # this returns only a list of the genres of each tv show
+                    genre_list.append(genre.text.strip())
+            item_info['Genres'] = ', '.join(genre_list)
+            starring_list = []
+            for index, starring in enumerate(soup.find_all('div', class_='summary_cast details_section')):
+                for name in starring.find_all('a'):
+                    starring_list.append(name.text.strip())
+            item_info['Starring'] = ', '.join(starring_list)
             for summary in soup.find_all('div', class_='summary_deck details_section', limit=1):
                 item_info['Summary'] = list(list(summary.children)[3].children)[1].text
             print(item_info)
         except IOError:
             logging.error(f'unable to find page to scrape url incorrect!')
 
-    def parallel_page_scraper(self):
+    def parallel_movie_scraper(self):
         """
         For each url extracted by search_pages_url, this method extracts film name and director.
         works in a batch method (sends a batch to be extracted)
@@ -171,14 +176,62 @@ class Scrapper:
                 continue
             logging.info(f'scraping a batch of urls')
 
+    def parallel_tv_show_scraper(self):
+        """
+        For each url extracted by search_pages_url, this method extracts film name and director.
+        works in a batch method (sends a batch to be extracted)
+        :return:
+        """
+        item_info = {}
+        page = (grequests.get(u, headers=cfg.USER_AGENT) for u in self.url_list)
+        response = grequests.map(page, size=cfg.BATCH_SIZE)
+        logging.info(f'successfully created url batch list')
+        for the_number, res in enumerate(response):
+            try:
+                soup = BeautifulSoup(res.content, 'lxml')
+                critic_meta_score_expression = re.compile('metascore_w larger')  # expression to find the meta score
+                for critic_ttl_score in soup.find_all('span', class_=critic_meta_score_expression, limit=1):
+                    item_info['Metascore'] = critic_ttl_score.text
+                user_score_expression = re.compile('metascore_w user')  # expression to find the user score
+                for user_ttl_score in soup.find_all('span', class_=user_score_expression, limit=1):
+                    item_info['User score'] = user_ttl_score.text
+                for title in soup.find_all('h1'):
+                    item_info['Title'] = title.text
+                # TODO:the year loop need to fix to take only second element
+                for release_year in soup.find('span', class_='release_date').find_all('span'):
+                    item_info['Release Year'] = release_year.text
+                studio_expression = re.compile('/company')  # expression to find the studio
+                for studio in soup.find_all('a', href=studio_expression, limit=1):
+                    item_info['Studio'] = studio.text
+                # TODO: Need to check multiple creators
+                for creator in soup.find_all('div', class_='creator', limit=1):
+                    item_info['Creator'] = list(list(creator.children)[3].children)[0].text
+                genre_list = []
+                for index, genre in enumerate(soup.find('div', class_='genres').find_all('span')):
+                    if index != 0 and index != 1:  # this returns only a list of the genres of each tv show
+                        genre_list.append(genre.text.strip())
+                item_info['Genres'] = ', '.join(genre_list)
+                starring_list = []
+                for index, starring in enumerate(soup.find_all('div', class_='summary_cast details_section')):
+                    for name in starring.find_all('a'):
+                        starring_list.append(name.text.strip())
+                item_info['Starring'] = ', '.join(starring_list)
+                for summary in soup.find_all('div', class_='summary_deck details_section', limit=1):
+                    item_info['Summary'] = list(list(summary.children)[3].children)[1].text
+                print(item_info)
+            except AttributeError:
+                logging.error(f'unable send batch to scrape')
+                continue
+            logging.info(f'scraping a batch of urls')
+
 
 def main():
-    the_scraper = Scrapper(cfg.EXAMPLE_WEB_PAGE)
+    the_scraper = Scrapper(cfg.EXAMPLE_WEB_PAGE_TV_SHOWS)
     part_1_seconds_before = time.time()
-    the_scraper.concurrent_page_scraping()
+    # the_scraper.concurrent_page_scraping()
     part_1_seconds_after = time.time()
     part_2_seconds_before = time.time()
-    the_scraper.parallel_page_scraper()
+    the_scraper.parallel_tv_show_scraper()
     part_2_seconds_after = time.time()
     print(f"part 1 time :{part_1_seconds_after - part_1_seconds_before}")
     print(f"part 2 time :{part_2_seconds_after-part_2_seconds_before}")
--- config.py
+++ config.py
@@ -2,6 +2,10 @@ MAIN_WEB_PAGE = 'https://www.metacritic.com'
 # Anchor page and subset of items used
 EXAMPLE_WEB_PAGE = 'https://www.metacritic.com/browse/movies/score/metascore/year' \
                    '/filtered?year_selected=2017&sort=desc&view=detailed'
+EXAMPLE_WEB_PAGE_GAMES = 'https://www.metacritic.com/browse/games/score/metascore/year' \
+                         '/all/filtered?year_selected=2020&distribution=&sort=desc&view=detailed'
+EXAMPLE_WEB_PAGE_TV_SHOWS = 'https://www.metacritic.com/browse/tv/score/metascore/year' \
+                            '/filtered?year_selected=2015&sort=desc&view=detailed'
 FILM_NAME_WITHOUT_YEAR = 0
 BATCH_SIZE = 5
 USER_AGENT = {'User-agent': 'Mozilla/5.0'}
