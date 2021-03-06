21549f911b7cb424204bfa0e8d470d0879f1a845
Ari Salkow <asalkow@gmail.com>
2021-03-06 12:13:14 +0200

    Post merge of DB_Construction with main.

--- URL_scraper.py
+++ URL_scraper.py
@@ -10,7 +10,7 @@ import re
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-# Create Formatter
+# Create formatter
 formatter = logging.Formatter('%(asctime)s-FILE:%(filename)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s')
 
 # Create a file handler and add it to logger
